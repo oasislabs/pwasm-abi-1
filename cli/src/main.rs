ff43fca6737ca5f54b59acf5f830609b9068bcc0
David Yan <david.yan@berkeley.edu>
2018-12-06 01:47:57 +0000

    lib.rs almost done generating

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,5 @@
+#![recursion_limit = "192"]
+
 extern crate proc_macro2;
 #[macro_use]
 extern crate quote;
@@ -8,11 +10,17 @@ use std::fs::File;
 use std::io::Read;
 
 use clap::{App, Arg};
-use proc_macro2::{TokenStream, Span};
+use proc_macro2::{TokenStream, Span, Group};
 use syn::{Attribute, Path, PathArguments, ItemTrait, PathSegment, Ident};
 use syn::token::{Pound, Bracket, Colon2};
 use syn::AttrStyle::Outer;
 use syn::punctuated::Punctuated;
+
+macro_rules! format_ident {
+  ($ident:expr, $fstr:expr) => {
+      syn::Ident::new(&format!($fstr, $ident), $ident.span())
+    };
+}
 
 fn main() {
     let matches = App::new("owasm-derive")
@@ -49,11 +57,11 @@ fn main() {
         tts: TokenStream::new(),
     };
 
-    let mut traits_to_method_sigs: Vec<(String, Vec<TokenStream>)> =
+    let mut traits_to_method_sigs: Vec<(&Ident, Vec<TokenStream>)> =
         ast.items.iter().filter_map(|item| match item {
             syn::Item::Trait(m) => {
                 if m.attrs.contains(&contract_attribute) {
-                    let trait_name = m.ident.to_string();
+                    let trait_name = &m.ident;
 
                     let method_sigs = m.items.iter().filter_map(|item| {
                         match item {
@@ -75,33 +83,43 @@ fn main() {
             _ => None,
         }).collect();
 
-    let mut contract_interface: String = "".to_owned();
-
     for (trait_name, method_sigs) in traits_to_method_sigs {
-        contract_interface.push_str(&trait_name);
-        contract_interface.push_str("\n");
+        let contract_endpoint = format_ident!(trait_name, "{}Endpoint");
+        let contract_client = format_ident!(trait_name, "{}Client");
+        let contract_struct = format_ident!(trait_name, "{}Inst");
+        let contract_interface = format_ident!(trait_name, "{}Interface");
+
+        let mut methods_stream = quote!();
         for method_sig in method_sigs {
-            contract_interface.push_str(&method_sig.to_string());
-            contract_interface.push_str("\n");
+            methods_stream.extend(method_sig);
         }
-    }
+        let methods_group = Group::new(proc_macro2::Delimiter::Brace, methods_stream);
+        let mut trait_stream = quote! {
+            extern crate owasm_abi;
+            extern crate owasm_abi_derive;
+            extern crate owasm_ethereum;
 
-    println!("{}", contract_interface);
+            use owasm_abi::eth::EndpointInterface;
+            use owasm_abi::types::*;
+            use owasm_abi_derive::eth_abi;
 
-/*
-    for tr in traits {
-        if tr.attrs.contains(&contract_attribute) {
-            println!("{:?}", tr.ident);
+            #[eth_abi(#contract_endpoint, #contract_client)]
+            trait #trait_name #methods_group
 
-            for item in &tr.items {
-                match item {
-                    syn::TraitItem::Method(m) => {
-                        println!("{:?}", m.sig);
-                    }
-                    _ => println!("not method"),
+            pub trait #contract_interface #methods_group
+
+            pub struct #contract_struct<T: #contract_interface>(pub T);
+
+            impl<T: #contract_interface> #contract_struct<T> {
+                pub fn deploy(self) {
+                    #contract_endpoint::new(self).dispatch_ctor(&owasm_ethereum::input());
+                }
+                pub fn call(self) {
+                    owasm_ethereum::ret(&contract_endpoint::new(self).dispatch(&owasm_ethereum::input()));
                 }
             }
-        }
+        };
+
+        println!("{}", trait_stream.to_string());
     }
-*/
 }

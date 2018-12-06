722d4d9a16856202592eb2cc8b1c1fe74f4cdaa9
David Yan <david.yan@berkeley.edu>
2018-12-06 21:41:59 +0000

    Write generated lib.rs to file

---
+++ cli/src/format.rs
@@ -0,0 +1,111 @@
+/// Copyright (c) 2013, Jyun-Yan You
+///
+/// All rights reserved.
+///
+/// Redistribution and use in source and binary forms, with or without
+/// modification, are permitted provided that the following conditions
+/// are met:
+///
+/// 1. Redistributions of source code must retain the above copyright
+///    notice, this list of conditions and the following disclaimer.
+/// 2. Redistributions in binary form must reproduce the above copyright
+///    notice, this list of conditions and the following disclaimer in the
+///    documentation and/or other materials provided with the distribution.
+/// 3. Neither the name of the author nor the names of his contributors
+///    may be used to endorse or promote products derived from this software
+/// without specific prior written permission.
+///
+/// THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND
+/// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+/// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+/// ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
+/// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+/// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+/// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+/// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+/// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+/// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+/// SUCH DAMAGE.
+use std::io::{self, Read, Write};
+use std::process;
+use std::sync::{Once, ONCE_INIT};
+
+// This function was copied from the following file:
+// https://github.com/rust-lang/rust-bindgen/blob/c3955ddeac236f4a6b967d5b8ddc8dc21238d152/tests/tests.rs
+//
+// Run `rustfmt` on the given source string and return a tuple of the formatted
+// bindings, and rustfmt's stderr.
+pub fn rustfmt(source: String) -> (String, String) {
+    static CHECK_RUSTFMT: Once = ONCE_INIT;
+
+    CHECK_RUSTFMT.call_once(|| {
+        let have_working_rustfmt = process::Command::new("rustup")
+            .args(&["run", "nightly", "rustfmt", "--version"])
+            .stdout(process::Stdio::null())
+            .stderr(process::Stdio::null())
+            .status()
+            .ok()
+            .map_or(false, |status| status.success());
+
+        if !have_working_rustfmt {
+            panic!(
+                "
+The latest `rustfmt` is required to run the `bindgen` test suite. Install
+`rustfmt` with:
+    $ rustup update nightly
+    $ rustup run nightly cargo install -f rustfmt-nightly
+"
+            );
+        }
+    });
+
+    let mut child = process::Command::new("rustup")
+        .args(&[
+            "run",
+            "nightly",
+            "rustfmt",
+            "--config-path",
+            concat!(env!("CARGO_MANIFEST_DIR"), "/rustfmt.toml"),
+        ])
+        .stdin(process::Stdio::piped())
+        .stdout(process::Stdio::piped())
+        .stderr(process::Stdio::piped())
+        .spawn()
+        .expect("should spawn `rustup run nightly rustfmt`");
+
+    let mut stdin = child.stdin.take().unwrap();
+    let mut stdout = child.stdout.take().unwrap();
+    let mut stderr = child.stderr.take().unwrap();
+
+    // Write to stdin in a new thread, so that we can read from stdout on this
+    // thread. This keeps the child from blocking on writing to its stdout which
+    // might block us from writing to its stdin.
+    let stdin_handle = ::std::thread::spawn(move || stdin.write_all(source.as_bytes()));
+
+    // Read stderr on a new thread for similar reasons.
+    let stderr_handle = ::std::thread::spawn(move || {
+        let mut output = vec![];
+        io::copy(&mut stderr, &mut output).map(|_| String::from_utf8_lossy(&output).to_string())
+    });
+
+    let mut output = vec![];
+    io::copy(&mut stdout, &mut output).expect("Should copy stdout into vec OK");
+
+    // Ignore actual rustfmt status because it is often non-zero for trivial
+    // things.
+    let _ = child.wait().expect("should wait on rustfmt child OK");
+
+    stdin_handle
+        .join()
+        .expect("writer thread should not have panicked")
+        .expect("should have written to child rustfmt's stdin OK");
+
+    let bindings = String::from_utf8(output).expect("rustfmt should only emit valid utf-8");
+
+    let stderr = stderr_handle
+        .join()
+        .expect("stderr reader thread should not have panicked")
+        .expect("should have read child rustfmt's stderr OK");
+
+    (bindings, stderr)
+}
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,46 +3,50 @@
 extern crate proc_macro2;
 #[macro_use]
 extern crate quote;
-extern crate syn;
 extern crate clap;
+extern crate owasm_abi_cli;
+extern crate syn;
 
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::Read;
 
 use clap::{App, Arg};
-use proc_macro2::{TokenStream, Span, Group};
-use syn::{Attribute, Path, PathArguments, ItemTrait, PathSegment, Ident, MethodSig, Pat, FnArg};
-use syn::token::{Pound, Bracket, Colon2, Comma};
-use syn::AttrStyle::Outer;
+use owasm_abi_cli::rustfmt;
+use proc_macro2::{Group, Span, TokenStream};
 use syn::punctuated::Punctuated;
+use syn::token::{Bracket, Colon2, Comma, Pound};
+use syn::AttrStyle::Outer;
+use syn::{Attribute, FnArg, Ident, MethodSig, Pat, Path, PathArguments, PathSegment};
 
 macro_rules! format_ident {
-  ($ident:expr, $fstr:expr) => {
-      syn::Ident::new(&format!($fstr, $ident), $ident.span())
+    ($ident:expr, $fstr:expr) => {
+        syn::Ident::new(&format!($fstr, $ident), $ident.span())
     };
 }
 
 fn main() {
     let matches = App::new("owasm-derive")
-        .arg(Arg::with_name("code")
-             .index(1)
-             .required(true)
-             .help("Path to contract code"))
+        .arg(
+            Arg::with_name("code")
+                .index(1)
+                .required(true)
+                .help("Path to contract code"),
+        )
         .get_matches();
     let mut file = File::open(matches.value_of("code").unwrap()).unwrap();
     let mut code = String::new();
-    file.read_to_string(&mut code);
+    file.read_to_string(&mut code).unwrap();
 
     let ast = syn::parse_file(&code).unwrap();
 
-    let mut punctuated: Punctuated<PathSegment, Colon2>= Punctuated::new();
+    let mut punctuated: Punctuated<PathSegment, Colon2> = Punctuated::new();
     punctuated.push(PathSegment {
         ident: Ident::new("owasm_abi_derive", Span::call_site()),
-        arguments: PathArguments::None
+        arguments: PathArguments::None,
     });
     punctuated.push(PathSegment {
         ident: Ident::new("contract", Span::call_site()),
-        arguments: PathArguments::None
+        arguments: PathArguments::None,
     });
 
     let contract_attribute = Attribute {
@@ -51,36 +55,44 @@ fn main() {
         bracket_token: Bracket::default(),
         path: Path {
             leading_colon: None,
-            segments: punctuated
+            segments: punctuated,
         },
         tts: TokenStream::new(),
     };
 
-    let mut traits_to_method_sigs: Vec<(&Ident, Vec<(TokenStream, &MethodSig)>)> =
-        ast.items.iter().filter_map(|item| match item {
+    let traits_to_method_sigs: Vec<(&Ident, Vec<(TokenStream, &MethodSig)>)> = ast
+        .items
+        .iter()
+        .filter_map(|item| match item {
             syn::Item::Trait(m) => {
                 if m.attrs.contains(&contract_attribute) {
                     let trait_name = &m.ident;
 
-                    let method_sigs = m.items.iter().filter_map(|item| {
-                        match item {
+                    let method_sigs = m
+                        .items
+                        .iter()
+                        .filter_map(|item| match item {
                             syn::TraitItem::Method(m) => {
                                 let msig = &m.sig;
                                 let mattrs = &m.attrs;
-                                Some((quote! {
-                                    #(#mattrs)*
-                                    #msig;
-                                }, msig))
+                                Some((
+                                    quote! {
+                                        #(#mattrs)*
+                                        #msig;
+                                    },
+                                    msig,
+                                ))
                             }
                             _ => None,
-                        }
-                    }).collect();
+                        })
+                        .collect();
                     return Some((trait_name, method_sigs));
                 }
                 None
             }
             _ => None,
-        }).collect();
+        })
+        .collect();
 
     for (trait_name, method_sigs) in traits_to_method_sigs {
         let contract_endpoint = format_ident!(trait_name, "{}Endpoint");
@@ -96,47 +108,49 @@ fn main() {
             let method_ident = &method_sig.ident;
             let mut inputs_iter = method_sig.decl.inputs.iter();
             let self_ref_check = inputs_iter.next();
-            let self_ref_error = format!("ABI function `{}` must have `&mut self` as its first argument.",
-                                         method_ident.to_string());
+            let self_ref_error = format!(
+                "ABI function `{}` must have `&mut self` as its first argument.",
+                method_ident.to_string()
+            );
             match self_ref_check {
                 Some(syn::FnArg::SelfRef(ref selfref)) => {
                     if selfref.mutability.is_none() {
                         panic!(self_ref_error)
                     }
                 }
-                _ => panic!(self_ref_error)
+                _ => panic!(self_ref_error),
             }
 
-            let mut arguments_group: Punctuated<Ident, Comma> = Punctuated::new();
+            let mut arguments_list: Punctuated<Ident, Comma> = Punctuated::new();
             for input in inputs_iter {
                 match input {
-                    FnArg::Captured(arg_captured) => {
-                        match &arg_captured.pat {
-                            Pat::Ident(pat_ident) => {
-                                arguments_group.push(pat_ident.ident.clone());
-                            }
-                            _ => (),
+                    FnArg::Captured(arg_captured) => match &arg_captured.pat {
+                        Pat::Ident(pat_ident) => {
+                            arguments_list.push(pat_ident.ident.clone());
                         }
-                    }
+                        _ => (),
+                    },
                     _ => (),
                 }
             }
-            if arguments_group.is_empty() {
+            if arguments_list.is_empty() {
                 contract_impl_stream.extend(quote! {
                     #method_sig {
-                        self.0.#method_ident()
+                        self . 0 . #method_ident()
                     }
                 });
             } else {
                 contract_impl_stream.extend(quote! {
                     #method_sig {
-                        self.0.#method_ident(#arguments_group)
+                        self . 0 . #method_ident(#arguments_list)
                     }
                 });
             }
         }
+
+        let contract_impl_group = Group::new(proc_macro2::Delimiter::Brace, contract_impl_stream);
         let methods_group = Group::new(proc_macro2::Delimiter::Brace, methods_stream);
-        let mut trait_stream = quote! {
+        let trait_stream = quote! {
             extern crate owasm_abi;
             extern crate owasm_abi_derive;
             extern crate owasm_ethereum;
@@ -160,8 +174,15 @@ fn main() {
                     owasm_ethereum::ret(&contract_endpoint::new(self).dispatch(&owasm_ethereum::input()));
                 }
             }
+
+            impl<T: #contract_interface> #trait_name for #contract_struct<T> #contract_impl_group
         };
 
-        //println!("{}", trait_stream.to_string());
+        let (output, err) = rustfmt(trait_stream.to_string());
+        if !err.is_empty() {
+            panic!("Failed to format the code: {}", err);
+        }
+        fs::create_dir_all("generated/src").unwrap();
+        fs::write("generated/src/lib.rs", output).unwrap();
     }
 }
